93a45ad66f4eabfdf4d57109eee92886b1f31de1
BrownSuga <arkamjaved@gmail.com>
2019-10-16 21:35:50 -0400

    stoney

--- q7/CustomTopo.py
+++ q7/CustomTopo.py
@@ -1,10 +1,4 @@
-from mininet.link import TCLink
-from mininet.log import setLogLevel
-from mininet.net import Mininet
-from mininet.node import CPULimitedHost
 from mininet.topo import Topo
-from mininet.util import dumpNodeConnections, irange
-
 
 class CustomTopo(Topo):
     "Simple Data Center Topology"
@@ -14,20 +8,31 @@ class CustomTopo(Topo):
     def __init__(self, linkopts1, linkopts2, linkopts3, fanout=2, **opts):
         # Initialize topology and default options
         Topo.__init__(self, **opts)
-        
         # Add your logic here ...
-        for i in range(fanout):
-            host1 = self.addHost('h%s' % i)
-            host2 = self.addHost('h%s' % i)
-            aggregate_switch = self.addSwitch('s%s' % i)
-            edge_switch = self.addSwitch('s%s' % i)
-            self.addLink(aggregate_switch, edge_switch, linkopts1["bw"], linkopts1["delay"])
-            self.addLink(edge_switch, host1, linkopts2["bw"], linkopts2["delay"])
-            self.addLink(edge_switch, host2, linkopts3["bw"], linkopts3["delay"])
-            
+        # Make Core 
+        core = self.addSwitch('c1')
+        edge_num = 1
+        host_num = 1
 
-linkopts1 = {"bw":10, "delay":'5ms', "loss":10, "max_queue_size":1000, "use_htb":True}
-linkopts2 = {"bw":10, "delay":'5ms', "loss":10, "max_queue_size":1000, "use_htb":True}
-linkopts3 = {"bw":10, "delay":'5ms', "loss":10, "max_queue_size":1000, "use_htb":True}
-                    
-topos = { 'custom': ( lambda: CustomTopo(linkopts1,linkopts2,linkopts3) ) }
+        # Build aggregation layer
+        for i in range(1, fanout + 1):
+            aggregation_switch = self.addSwitch('a%s' % i)
+            self.addLink(aggregation_switch, core,  **linkopts1)
+
+            # Build edge layer
+            for j in range (1, fanout + 1):
+                edge_switch = self.addSwitch('e%s' % edge_num)
+                edge_num += 1
+                self.addLink(edge_switch, aggregation_switch,  **linkopts2)
+
+                # Build host layer
+                for j in range (1, fanout + 1):
+                    host = self.addHost('h%s' % host_num)
+                    host_num += 1
+                    self.addLink(host, edge_switch, **linkopts3)
+
+linkopts1 = dict(bw=10, delay='5ms', max_queue_size=1000, use_htb=True)
+linkopts2 = dict(bw=10, delay='5ms', max_queue_size=1000, use_htb=True)
+linkopts3 = dict(bw=10, delay='5ms', max_queue_size=1000, use_htb=True)
+
+topos = { 'custom': ( lambda: CustomTopo(linkopts1,linkopts2,linkopts3)) }
