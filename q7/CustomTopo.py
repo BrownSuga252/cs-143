bd3c5bc66ac31c760f3a6d942f973b9119bfbcff
BrownSuga <arkamjaved@gmail.com>
2019-10-16 21:07:36 -0400

    nice6

--- q7/CustomTopo.py
+++ q7/CustomTopo.py
@@ -21,9 +21,9 @@ class CustomTopo(Topo):
             host2 = self.addHost('h%s' % i)
             aggregate_switch = self.addSwitch('s%s' % i)
             edge_switch = self.addSwitch('s%s' % i)
-            self.addLink(aggregate_switch, edge_switch, linkopts1["bw"], linkopts1["delay"],linkopts1["loss"], linkopts1["max_queue_size"], linkopts1["use_htb"] )
-            self.addLink(edge_switch, host1, linkopts2["bw"], linkopts2["delay"],linkopts2["loss"], linkopts2["max_queue_size"], linkopts2["use_htb"])
-            self.addLink(edge_switch, host2, linkopts3["bw"], linkopts3["delay"],linkopts3["loss"], linkopts3["max_queue_size"], linkopts3["use_htb"])
+            self.addLink(aggregate_switch, edge_switch, linkopts1["bw"], linkopts1["delay"])
+            self.addLink(edge_switch, host1, linkopts2["bw"], linkopts2["delay"])
+            self.addLink(edge_switch, host2, linkopts3["bw"], linkopts3["delay"])
             
 
 linkopts1 = dict(bw=10, delay='5ms', loss=10, max_queue_size=1000, use_htb=True)
