f6f6234d0698dae4f1e5f4912d2182b9a9577599
BrownSuga <arkamjaved@gmail.com>
2019-10-16 21:54:04 -0400

    stoneyeditl

--- q7/CustomTopo.py
+++ q7/CustomTopo.py
@@ -1,16 +1,20 @@
 from mininet.topo import Topo
 
+
 class CustomTopo(Topo):
     "Simple Data Center Topology"
 
     "linkopts - (1:core, 2:aggregation, 3: edge) parameters"
     "fanout - number of child switch per parent switch"
+
     def __init__(self, linkopts1, linkopts2, linkopts3, fanout=2, **opts):
         # Initialize topology and default options
         Topo.__init__(self, **opts)
-        # Add your logic here ...
-        # Make Core 
+
+        # Build core layer
         core = self.addSwitch('c1')
+
+        # Keep track of which edge and host node we are on
         edge_num = 1
         host_num = 1
 
@@ -20,19 +24,20 @@ class CustomTopo(Topo):
             self.addLink(aggregation_switch, core,  **linkopts1)
 
             # Build edge layer
-            for j in range (1, fanout + 1):
+            for j in range(1, fanout + 1):
                 edge_switch = self.addSwitch('e%s' % edge_num)
                 edge_num += 1
                 self.addLink(edge_switch, aggregation_switch,  **linkopts2)
 
                 # Build host layer
-                for j in range (1, fanout + 1):
+                for j in range(1, fanout + 1):
                     host = self.addHost('h%s' % host_num)
                     host_num += 1
                     self.addLink(host, edge_switch, **linkopts3)
+
 
 linkopts1 = dict(bw=10, delay='5ms', max_queue_size=1000, use_htb=True)
 linkopts2 = dict(bw=10, delay='5ms', max_queue_size=1000, use_htb=True)
 linkopts3 = dict(bw=10, delay='5ms', max_queue_size=1000, use_htb=True)
 
-topos = { 'custom': ( lambda: CustomTopo(linkopts1,linkopts2,linkopts3)) }
+topos = {'custom': (lambda: CustomTopo(linkopts1, linkopts2, linkopts3))}
